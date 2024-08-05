07301b6b4eca3cb650ccc4ec74592bae3a212289
Jakub Drdak <jak.drd@gmail.com>
2024-08-05 16:49:01 +0200

    create AWS composer

--- backend/src/generate_response/lambda_function.py
+++ backend/src/generate_response/lambda_function.py
@@ -4,16 +4,17 @@ from agents.graph import build_graph
 from config import DEBUG_MODE
 
 def lambda_handler(event, context):
-
-    event_body = json.loads(event["body"])
-    human_input = event_body["prompt"]
+    print("Event: ", event)
+    print("Context: ", context)
+    # event_body = json.loads(event["body"])
+    # human_input = event_body["prompt"]
     # Get the environment variables
     # bucket_name = os.environ['BUCKET_NAME']
     # key = os.environ['KEY']
 
     graph = build_graph()
 
-    task = "Dezinformátorské žně kvůli ukrajinskému obilí. EU přitom nezavádí omezení dovozu kvůli jedům"
+    human_input = "Dezinformátorské žně kvůli ukrajinskému obilí. EU přitom nezavádí omezení dovozu kvůli jedům"
     params = {
         'task': human_input,
         'target_language': "czech",
