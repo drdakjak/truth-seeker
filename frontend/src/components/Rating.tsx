f8fd31910b575f3c5cb51a7f83288df8ffb07e5f
Jakub Drdak <jak.drd@gmail.com>
2024-08-31 21:58:01 +0200

    update rating icons

--- frontend/src/components/Rating.tsx
+++ frontend/src/components/Rating.tsx
@@ -10,21 +10,21 @@ const Rating = ({ rating, handleRating }) => {
 
             <button
                 onClick={() => handleRating(0)}
-                className={`mr-2 p-2 rounded bg-indigo-600 ${rating === 0 ? 'bg-indigo-900' : 'bg-indigo-600'}`}
+                className={`mr-2 p-2 hover:scale-110 rounded bg-indigo-600 ${rating === 0 ? 'bg-indigo-900' : 'bg-indigo-600'}`}
             >
-                <FaFaceFrown className="text-2xl  text-yellow-300" />
+                <FaFaceFrown className="text-2xl text-yellow-300" />
             </button>
 
             <button
                 onClick={() => handleRating(1)}
-                className={`mr-2 p-2 rounded bg-indigo-600 ${rating === 1 ? 'bg-indigo-900' : 'bg-indigo-600'}`}
+                className={`mr-2 p-2 hover:scale-110 rounded bg-indigo-600 ${rating === 1 ? 'bg-indigo-900' : 'bg-indigo-600'}`}
             >
                 <FaFaceSmile className="text-2xl  text-yellow-300" />
             </button>
             
             <button
                 onClick={() => handleRating(2)}
-                className={`p-2 rounded  bg-indigo-600 ${rating === 2 ? 'bg-indigo-900' : 'bg-indigo-600'}`}
+                className={`p-2 hover:scale-110 rounded  bg-indigo-600 ${rating === 2 ? 'bg-indigo-900' : 'bg-indigo-600'}`}
             >
                 <FaFaceGrinHearts className="text-2xl text-yellow-300" />
             </button>
